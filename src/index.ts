ff523fd6954bef959eb7cefce455471117e5f8f1
Duy Tran <duytran31187@gmail.com>
2024-05-27 21:28:36 +0700

    nameOfDays

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,34 @@ import { NamPhungVu } from "./commonData";
 import { tinh4TuanMuaVong, tinhNamABC } from "./tinh4TuanMuaVong";
 import { tinhLeChuaHienLinh, tinhThuTuLeTro, tinhNgayPhucSinh, tinhLeThanhGia, tinhLeChuaChiuPhepRua } from "./tinhlephucsinh";
 import { addDate, getChristmasDay } from "./utils";
+export const nameOfDays = {
+    year: 'year( Năm)',
+    yearABC: 'A|B|C (năm A|B|C)',
+    oddEven: 'Odd|Even (Năm chẵn lẻ)',
+    theEpiphanyOfTheLord: 'The Epiphany of the Lord (Lễ Chúa Hiển Linh)',
+    leChuaChiuPhepRua: 'Lễ Chúa chịu phép rửa',
+    ashWed: 'Ash Wednesday (Thứ tư lễ tro)',
+    firstSundayOfLent: 'First Sunday of Lent (Chúa nhật thứ nhất mùa chay)',
+    secondSundayOfLent: 'Second Sunday of Lent (Chúa nhật thứ 2 mùa chay)',
+    thirdSundayOfLent: 'Third Sunday of Lent (Chúa nhật thứ 3 mùa chay)',
+    fourthSundayOfLent: 'Fourth Sunday of Lent (Chúa nhật thứ 4 mùa chay)',
+    fifthSundayOfLent: 'Fifth Sunday of Lent (Chúa nhật thứ 5 mùa chay)',
+    palmSunday: 'Palm Sunday (Lễ Lá)',
+    easterSunday: 'Easter Sunda (Phục sinh)',
+    secondSundayOfEaster: 'Second Sunday of Easter (Chúa nhật thứ 2 phục sinh)',
+    thirdSundayOfEaster: 'Third Sunday of Easter (Chúa nhật thứ 3 phục sinh)',
+    fourthSundayOfEaster: 'Fourth Sunday of Easter (Chúa nhật thứ 4 phục sinh)',
+    fifthSundayOfEaster: 'Fifth Sunday of Easter (Chúa nhật thứ 5 phục sinh)',
+    sixthSundayOfEaster: 'Sixth Sunday of Easter (Chúa nhật thứ 6 phục sinh)',
+    theAscentionOfTheLord: 'The Ascention of the Lord (Lễ Chúa Lên Trời)',
+    pentecostSunday: 'Pentecost Sunday (Lễ Chúa Thánh Thần hiện xuống)',
+    firstSundayOfAdvent: 'First Sunday of Advent (Chúa nhật thứ nhất mùa vọng)',
+    secondSundayOfAdvent: 'Second Sunday of Advent (Chúa nhật thứ 2 mùa vọng)',
+    thirdSundayOfAdvent: 'Third Sunday of Advent (Chúa nhật thứ 3 mùa vọng)',
+    fourthSundayOfAdvent: 'Fourth Sunday of Advent (Chúa nhật thứ tư mùa vọng)',
+    christmas: 'Christmas (Giáng sinh)',
+    leThanhGia: 'Lễ Thánh Gia'
+};
 
 export function tinhNamPhungVu(y: number): NamPhungVu {
     const tuanmuaVong = tinh4TuanMuaVong(y);
@@ -36,3 +64,9 @@ export function tinhNamPhungVu(y: number): NamPhungVu {
         leThanhGia: tinhLeThanhGia(y)
     }
 }
+// const namphungVuIns = tinhNamPhungVu(2024);
+// for( let key in namphungVuIns) 
+// {
+//     const val = namphungVuIns[key] instanceof Date ? namphungVuIns[key].toDateString() : namphungVuIns[key];
+//     console.log(`${nameOfDays[key]}: ${val}`);
+// }
